626047d191fa9e5726ca2378999e723da57fff0a
ElectricPie <32139670+ElectricPie@users.noreply.github.com>
2017-10-27 07:37:44 +0100

    Design 1 + 2 Updates
    The door for design 1 now has textures and is now operated by the player interacting with the door by press ‘e’.
    Desk, Monitor, Keyboard, Computer and Keyboard models are now in Unity however they currently don’t have textures.

---
+++ Assets/Design 1/Scripts/DoorAnimation.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorAnimation : MonoBehaviour {
+
+    [SerializeField]
+    private AudioClip m_open_s;
+
+    [SerializeField]
+    private AudioClip m_close_s;
+
+    [SerializeField]
+    private AudioClip m_keypad_s;
+
+    private Animator m_anim;
+    private AudioSource m_audio;
+    private bool m_open;
+    private bool m_active;
+
+    // Use this for initialization
+    void Start () {
+        m_open = false;
+        m_active = false;
+
+        m_anim = GetComponent<Animator>();
+        m_audio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        if (m_active == false)
+        {
+            //Debug.Log("not active");
+            
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                m_active = true;
+                if (m_open == true)
+                {
+                    Invoke("Open", 1);
+                }
+                else
+                {
+                    m_audio.PlayOneShot(m_keypad_s, 1);
+                    Invoke("Close", 1.7f);
+                }
+            }
+        }
+        else
+        {
+            //Debug.Log("Active");
+        }
+        */
+    }
+
+    private void Open()
+    {
+        m_open = false;
+        m_anim.SetBool("Open", false);
+        m_audio.PlayOneShot(m_open_s, 1);
+        m_active = false;
+    }
+
+    private void Close()
+    {
+        m_open = true;
+        m_anim.SetBool("Open", true);
+        m_audio.PlayOneShot(m_close_s, 1);
+        m_active = false;
+    }
+
+    public void Transition()
+    {
+        if (m_open)
+        {
+            Invoke("Open", 1);
+        }
+        else
+        {
+            m_audio.PlayOneShot(m_keypad_s, 1);
+            Invoke("Close", 1.7f);
+        }
+    }
+}
---
+++ Assets/Design 1/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public DoorAnimation animCont;
+
+    public float sensitivity = 1;
+
+    [SerializeField]
+    private float m_speed = 0.8f;
+
+    [SerializeField]
+    private float m_jump = 5;
+
+    private float m_interactDelay;
+
+    private Rigidbody m_rigidbody;
+
+    private bool m_grounded;
+    private bool m_holdingObj = false;
+
+    Vector3 temp;
+
+    // Use this for initialization
+    void Start () {
+        m_rigidbody = GetComponent<Rigidbody>();
+	}
+
+    // Update is called once per frame
+    void Update() {
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+        float xMouseInput = Input.GetAxis("Mouse X") * sensitivity;
+
+        temp = (transform.right * xInput + transform.forward * zInput) * m_speed;
+
+        if (temp.x > 0.2)
+            temp.x = 0.2f;
+
+        if (temp.x < -0.2)
+            temp.x = -0.2f;
+
+        if (temp.z > 0.2)
+            temp.z = 0.2f;
+
+        if (temp.z < -0.2)
+            temp.z = -0.2f;
+
+        //Jumping
+        if (Input.GetAxis("Jump") == 1 && m_grounded == true)
+        {
+            temp.y += m_jump;
+            m_grounded = false;
+        }
+
+        //Interacting
+        if (Input.GetAxis("Interact") == 1 && m_interactDelay <= 0)
+        {
+
+            Transform cam = Camera.main.transform;
+            Ray ray = new Ray(cam.position, cam.forward);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                print(hit.collider);
+
+                if (hit.collider.tag == "Door")
+                {
+                    animCont.Transition();
+                }
+            }
+
+            m_interactDelay = 1;
+        }
+
+
+        m_rigidbody.velocity += temp;
+
+        if (m_interactDelay > 0)
+        {
+            m_interactDelay -= Time.deltaTime * 1;
+        }
+
+        transform.Rotate(0, xMouseInput, 0);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        m_grounded = true;
+    }
+}
+
